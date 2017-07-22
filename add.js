52ebf1e06041f39d4c4a44ac5ead3ee76fd7adb4
ledet awano <ledet2016@gmail.com>
2017-07-21 22:41:21 -0500

    Create add.js

---
+++ add.js
@@ -0,0 +1,7 @@
+Write a function that returns the sum of two numbers.
+
+function add(param1, param2) { // function with two parameters 
+ return param1 + param2; // return statement that will add the value of the arguments passed 
+}
+
+add(1,2); // 3
